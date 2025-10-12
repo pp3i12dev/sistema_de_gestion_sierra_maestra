fb2f01f4b965105773b98c963c7866aca807ada9
Alfredo <pp3i12.dev@gmail.com>
2025-10-12 12:20:02 -0300

    Actualización y mejoras, nuevo botón de Descargar Excel

--- src/main/java/com/sca/repository/MaduradorRepository.java
+++ src/main/java/com/sca/repository/MaduradorRepository.java
@@ -1,5 +1,7 @@
 package com.sca.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.sca.model.Madurador;
 
 @Repository
 public interface MaduradorRepository  extends JpaRepository <Madurador, Long>{
-
+    // Este método nos permitirá obtener todos los Maduradores de un estado específico
+    List<Madurador> findByEstado(String estado);
 }
